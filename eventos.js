727fb645abce683a1ce6a6ee0bdb95f39b4e144e
DamianUY <damieva2021@gmail.com>
2023-08-13 19:04:26 -0300

    Cartel del div

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var DivDeAlerta = document.getElementById("ElDiv");
+    
+    DivDeAlerta.addEventListener("click", function() {
+        alert("Hola! Soy el div");
+    });
+});
